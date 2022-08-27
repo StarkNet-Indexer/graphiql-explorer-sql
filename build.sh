09846413e5696d430f172e96426bca804adeebff
Oleg Abdrashitov <olegabu73@gmail.com>
2022-08-27 17:17:35 +0400

    testing workflow

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export REACT_APP_SQL_URL="https://hasura.$namespace.summary.dev/sql"
 export PUBLIC_URL="https://$namespace.summary.dev/console"
 
 # default query for the GraphQL editor
-export REACT_APP_DEFAULT_GRAPHQL_QUERY="{ event(limit: 10) { name } }"
+export REACT_APP_DEFAULT_GRAPHQL_QUERY="# $namespace $sha_short \n\n { event(limit: 10) { name } }"
 # default query for the SQL editor
 export REACT_APP_DEFAULT_SQL_QUERY="select * from event limit 10"
 
