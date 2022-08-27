f1fd43a5f736d84e10d0c1dac5e9c24051e2a00c
Oleg Abdrashitov <olegabu73@gmail.com>
2022-08-27 17:49:07 +0400

    added GRAPHQL_WELCOME

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ export PUBLIC_URL="https://$namespace.summary.dev/console"
 export REACT_APP_DEFAULT_GRAPHQL_QUERY="{ event(limit: 10) { name } }"
 # default query for the SQL editor
 export REACT_APP_DEFAULT_SQL_QUERY="select * from event limit 10"
-export REACT_APP_GRAPHQL_WELCOME="Welcome $namespace $sha_short"
+export REACT_APP_GRAPHQL_WELCOME="Welcome to StarkNet Indexer $namespace $sha_short"
 
 env | grep REACT_APP
 echo PUBLIC_URL=$PUBLIC_URL
