6ea23dd220e3051b96a44349dc1e48dcf4a9f694
Oleg Abdrashitov <olegabu73@gmail.com>
2022-08-28 11:01:10 +0400

    cleaned up

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo namespace=$namespace
+env | grep namespace
 
 kubectl exec --namespace $namespace -it data-nginx -- rm -rf data/console && \
 kubectl cp --namespace $namespace build data-nginx:/data/console
