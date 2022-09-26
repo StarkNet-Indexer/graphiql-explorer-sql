f07d047f1adec80efa136c6bf8b80df12379c92d
Oleg Abdrashitov <olegabu73@gmail.com>
2022-09-26 18:29:09 +0400

    updated REACT_APP_DEFAULT_GRAPHQL_QUERY

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export REACT_APP_GRAPHQL_SUBSCRIPTION_URL="wss://hasura.$namespace.summary.dev/v
 export REACT_APP_SQL_URL="https://api.$namespace.summary.dev/sql-query"
 export PUBLIC_URL="https://${subdomain}summary.dev/console"
 
-export REACT_APP_DEFAULT_GRAPHQL_QUERY="{ starknet_goerli_block(limit: 1, order_by: {block_number: desc}) { block_number block_hash } starknet_mainnet_block(limit: 1, order_by: {block_number: desc}) { block_number block_hash } }"
+export REACT_APP_DEFAULT_GRAPHQL_QUERY="latest_blocks { starknet_goerli_block(limit: 1, order_by: {block_number: desc}) {block_number block_hash} starknet_mainnet_block(limit: 1, order_by: {block_number: desc}) {block_number block_hash} }"
 export REACT_APP_DEFAULT_SQL_QUERY="select * from starknet_goerli.event left join starknet_goerli.argument on starknet_goerli.argument.event_id = starknet_goerli.event.id order by starknet_goerli.event.id desc limit 10"
 export REACT_APP_GRAPHQL_WELCOME="Welcome to Summary $namespace $sha_short"
 
